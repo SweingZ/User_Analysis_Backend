4e90205c65c13dd85fe300034cdbb1f0d00785ea
Sweing Nakarmi <sweing111@gmail.com>
2024-11-13 15:54:53 +0545

    added path history and bounce

--- app/model/session_data.py
+++ app/model/session_data.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel
 
 class SessionData(BaseModel):
     session_start : Optional[datetime] = None
     session_end : Optional[datetime] = None
+    path_history : Optional[List[str]] = None
+    bounce : Optional[bool] = None

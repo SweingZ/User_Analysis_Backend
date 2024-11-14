9c287f434a8b49ec7864532745b7cf25cefa9a10
Sweing Nakarmi <sweing111@gmail.com>
2024-11-14 15:58:13 +0545

    Added device stats fields

--- app/model/session_data.py
+++ app/model/session_data.py
@@ -2,8 +2,16 @@ from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel
 
+class DeviceStats(BaseModel):
+    os : Optional[str] = None
+    browser : Optional[str] = None
+    device : Optional[str] = None
+
 class SessionData(BaseModel):
     session_start : Optional[datetime] = None
     session_end : Optional[datetime] = None
     path_history : Optional[List[str]] = None
     bounce : Optional[bool] = None
+    device_stats : Optional[DeviceStats] = None
+
+

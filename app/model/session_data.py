5d92777fd4af6df63489a78974476293b3e70387
Sweing Nakarmi <sweing111@gmail.com>
2024-11-15 15:39:58 +0545

    added location

--- app/model/session_data.py
+++ app/model/session_data.py
@@ -7,11 +7,16 @@ class DeviceStats(BaseModel):
     browser : Optional[str] = None
     device : Optional[str] = None
 
+class Location(BaseModel):
+    latitude : Optional[str] = None
+    longitude : Optional[str] = None
+
 class SessionData(BaseModel):
     session_start : Optional[datetime] = None
     session_end : Optional[datetime] = None
     path_history : Optional[List[str]] = None
     bounce : Optional[bool] = None
     device_stats : Optional[DeviceStats] = None
+    location : Optional[Location] = None
 
 

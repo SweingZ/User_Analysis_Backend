6285cbbf87a8b867fe3ab9e1093fc87b96896d4f
Sweing Nakarmi <sweing111@gmail.com>
2024-12-05 23:20:13 +0545

    refactor: minor changes in get_content_metrics_data (subsricption rate)

--- app/service/dashboard_service.py
+++ app/service/dashboard_service.py
@@ -136,18 +136,20 @@ class DashboardService:
                     "subscribers": metric.get("subscribers", 0),
                     "child_buttons": metric.get("child_buttons",{}),
                     "avg_watch_time": (metric.get("sum_watch_time", 0) / metric["views"]) if metric.get("views") else 0,
-                    "avg_completion_rate": (metric.get("sum_completion_rate", 0) / metric["views"]) if metric.get("views") else 0
+                    "avg_completion_rate": (metric.get("sum_completion_rate", 0) / metric["views"]) if metric.get("views") else 0,
+                    "subscription_rate": (metric.get("subscribers", 0) / metric["views"]) if metric.get("views") else 0
                 }
             elif metric["type"] == "CONTENT":
                 content_metrics[metric["title"]] = {
                     "views": metric.get("views", 0),
                     "likes": metric.get("likes", 0),
+                    "cta_clicks": metric.get("cta_clicks", 0),
+                    "subscribers": metric.get("subscribers", 0),
+                    "child_buttons": metric.get("child_buttons",{}),
                     "avg_scroll_depth": (metric.get("sum_scroll_depth", 0) / metric["views"]) if metric.get("views") else 0,
                     "avg_watch_time": (metric.get("sum_watch_time", 0) / metric["views"]) if metric.get("views") else 0,
                     "avg_completion_rate": (metric.get("sum_completion_rate", 0) / metric["views"]) if metric.get("views") else 0,
-                    "cta_clicks": metric.get("cta_clicks", 0),
-                    "subscribers": metric.get("subscribers", 0),
-                    "child_buttons": metric.get("child_buttons",{})
+                    "subscription_rate": (metric.get("subscribers", 0) / metric["views"]) if metric.get("views") else 0
                 }
             elif metric["type"] == "BUTTON":
                 button_clicks[metric["title"]] += metric.get("clicks", 0)

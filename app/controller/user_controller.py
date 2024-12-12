9ad91ded52aec396138b4d360a5a0b128925bd65
Sweing Nakarmi <sweing111@gmail.com>
2024-12-12 13:39:04 +0545

    feat: implemented auth in user controller

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -15,19 +15,21 @@ async def register_user(user: User):
 
 @user_route.get("/create_user")
 async def get_id():
-    user = await mongodb.collections["user"].insert_one({"session_ids": []})  # Await the insert operation
-    return {"user_id": str(user.inserted_id)}  # Convert ObjectId to string
+    user = await mongodb.collections["user"].insert_one({"session_ids": []})  
+    return {"user_id": str(user.inserted_id)}  
 
 @user_route.get("/get_top_users/{admin_id}")
-async def get_top_users(admin_id: str, payload: str = Depends(feature_access_verification)):
+async def get_top_users(admin_id: str):
+    await feature_access_verification(page_name="USER_STATS")
     users = await UserService.get_top_users(admin_id)
     return users
 
 @user_route.get("/sessions/{user_id}")
-async def get_user_session(
+async def get_user_session( 
     user_id: str,
     year: Optional[int] = Query(None),
     month: Optional[int] = Query(None)
 ):
+    await feature_access_verification(page_name="USER_STATS")
     session = await UserService.get_user_session(user_id, year, month)        
     return session

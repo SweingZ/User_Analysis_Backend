98a7d5dbf980fb9fb059ce59f8e71dce1f218d8f
Sweing Nakarmi <sweing111@gmail.com>
2024-12-27 16:11:06 +0545

    fixed user endpoints

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,12 +1,14 @@
 from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi.security import OAuth2PasswordBearer
 from app.model.user_model import User
 from app.service.user_service import UserService
 from app.config.db_config import mongodb
 from app.utils.jwt_utils import feature_access_verification
 
-
 user_route = APIRouter()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @user_route.post("/register")
 async def register_user(user: User):
@@ -19,8 +21,8 @@ async def get_id():
     return {"user_id": str(user.inserted_id)}  
 
 @user_route.get("/get_top_users/{admin_id}")
-async def get_top_users(admin_id: str):
-    await feature_access_verification(page_name="USER_STATS")
+async def get_top_users(admin_id: str, token: str = Depends(oauth2_scheme)):
+    await feature_access_verification(page_name="USER_STATS", token=token)
     users = await UserService.get_top_users(admin_id)
     return users
 
@@ -28,8 +30,9 @@ async def get_top_users(admin_id: str):
 async def get_user_session( 
     user_id: str,
     year: Optional[int] = Query(None),
-    month: Optional[int] = Query(None)
+    month: Optional[int] = Query(None),
+    token: str = Depends(oauth2_scheme)
 ):
-    await feature_access_verification(page_name="USER_STATS")
+    await feature_access_verification(page_name="USER_STATS", token=token)
     session = await UserService.get_user_session(user_id, year, month)        
     return session
